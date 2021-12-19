117108ee9bf51b081adc46ea35b6164c97e46c33
Kawano Kenichi <kenichi.k.820@gmail.com>
2021-12-19 06:23:39 +0000

    :sparkles: Add new feature
    ジョークの詳細を表示する。

--- src/routes/jokes/$jokeId.tsx
+++ src/routes/jokes/$jokeId.tsx
@@ -1,4 +1,26 @@
+import type { Joke } from '@prisma/client';
+import { Link, LoaderFunction, useLoaderData } from 'remix';
+
+import { db } from '~/utils/db.server';
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+
+  if (!joke) {
+    throw new Error('Joke not found');
+  }
+
+  const data: LoaderData = { joke };
+  return data;
+};
+
 const JokeRoute = () => {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here&apos;s your hilarious joke:</p>
@@ -6,6 +28,7 @@ const JokeRoute = () => {
         Why don&apos;t you find hippopotamuses hiding in trees? They&apos;re
         really good at it.
       </p>
+      <Link to='.'>{data.joke.name} Permalink</Link>
     </div>
   );
 };
