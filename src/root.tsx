137272d6f6b4a3f4769de66cb40bfd3e2c7cf493
Kawano Kenichi <kenichi.k.820@gmail.com>
2021-12-19 06:14:59 +0000

    :sparkles: Add new features

--- src/root.tsx
+++ src/root.tsx
@@ -1,3 +1,24 @@
+import type { LinksFunction } from 'remix';
+
 import { App } from './app';
+import globalLargeStylesUrl from './styles/global-large.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
+import globalStylesUrl from './styles/global.css';
 
 export default App;
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: globalStylesUrl },
+    {
+      rel: 'stylesheet',
+      href: globalMediumStylesUrl,
+      media: 'print, (min-width: 640px)',
+    },
+    {
+      rel: 'stylesheet',
+      href: globalLargeStylesUrl,
+      media: 'screen and (min-width: 1024px)',
+    },
+  ];
+};
---
+++ src/routes/jokes/$jokeId.tsx
@@ -0,0 +1,13 @@
+const JokeRoute = () => {
+  return (
+    <div>
+      <p>Here&apos;s your hilarious joke:</p>
+      <p>
+        Why don&apos;t you find hippopotamuses hiding in trees? They&apos;re
+        really good at it.
+      </p>
+    </div>
+  );
+};
+
+export default JokeRoute;
